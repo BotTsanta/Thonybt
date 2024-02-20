25ea849c7e2f1c85fa8600d58e022b722f37d083
tsta <157697467+tstrb@users.noreply.github.com>
2024-02-20 17:39:06 +0300

    Update and rename gpt to gpt.js

--- script/gpt.js
+++ script/gpt.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: 'gpt', //hercai
   version: '1.0.0',
   role: 0,
-  hasPrefix: false,
+  hasPrefix: true,
   description: "An AI command powered by TsantaBot",
   usage: "gpt [question]",
   credits: 'TsantaBot',
