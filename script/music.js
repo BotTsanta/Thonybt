8dea932a0159fab3769622de5bc1ce1cd35ce78d
tsta <157697467+tstrb@users.noreply.github.com>
2024-03-06 16:38:19 +0300

    Update music.js

--- script/music.js
+++ script/music.js
@@ -1,108 +1,60 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const ytdl = require("ytdl-core");
-const request = require("request");
-const yts = require("yt-search");
-
-
-module.exports = {
-  config: {
-    name: "music",
-    aliases: [],
-    version: "1.0",
-    role: 1,
-    credits: "TsantaBot",
-    cooldown: 30,
-    shortdescription: "send YouTube music",
-    longdescription: "",
-    category: "audio",
-    usages: "{pn} music name",
-    dependencies: {
-      "fs-extra": "",
-      "request": "",
-      "axios": "",
-      "ytdl-core": "",
-      "yt-search": ""
-    }
-  },
-
-
-  run: async ({ api, event }) => {
-    const input = event.body;
-    const text = input.substring(12);
-    const data = input.split(" ");
-
-
-    if (data.length < 2) {
-      return api.sendMessage("💡Ex: Music Mr Said Veloma \n\n 🆓️ Dispo isaky ny 3 minutes\n 🌐 bit.ly/tsantabot ", event.threadID);
-    }
-
-
-    data.shift();
-    const videoName = data.join(" ");
-
-
-    try {
-      api.sendMessage(`✅ | TsantaBot va chercher 《${videoName}》\n
-⏳ | Attendez svp...`, event.threadID);
-
-
-      const searchResults = await yts(videoName);
-      if (!searchResults.videos.length) {
-        return api.sendMessage("Aucune vidéo trouvée !", event.threadID, event.messageID);
-      }
-
-
-      const video = searchResults.videos[0];
-      const videoUrl = video.url;
-
-
-      const stream = ytdl(videoUrl, { filter: "audio" });
-
-
-      const fileName = `${event.senderID}.mp3`;
-      const filePath = __dirname + `/cache/${fileName}`;
-
-
+const path = require('path');
+module.exports.config = {
+  name: "music",
+  version: "1.0.0",
+  role: 0,
+  hasPrefix: false,
+  aliases: ['hira'],
+  usage: 'Music [promt]',
+  description: 'Search music in youtube',
+  credits: 'Deveploper',
+  cooldown: 60
+};
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const fs = require("fs-extra");
+  const ytdl = require("ytdl-core");
+  const yts = require("yt-search");
+  const musicName = args.join(' ');
+  if (!musicName) {
+    api.sendMessage(`▪︎Ex: Music Mr Said Veloma\n\n ▪︎Créez Chatbot sur bit.ly/tsantabot `, event.threadID, event.messageID);
+    return;
+  }
+  try {
+    api.sendMessage(`🔎| TsantaBot cherche 《${musicName}》 `, event.threadID, event.messageID);
+    const searchResults = await yts(musicName);
+    if (!searchResults.videos.length) {
+      return api.sendMessage("Aucun résultat.", event.threadID, event.messageID);
+    } else {
+      const music = searchResults.videos[0];
+      const musicUrl = music.url;
+      const stream = ytdl(musicUrl, {
+        filter: "audioonly"
+      });
+      const time = new Date();
+      const timestamp = time.toISOString().replace(/[:.]/g, "-");
+      const filePath = path.join(__dirname, 'cache', `${timestamp}_music.mp3`);
       stream.pipe(fs.createWriteStream(filePath));
-
-
-      stream.on('response', () => {
-        console.info('[DOWNLOADER]', 'Starting download now!');
-      });
-
-
-      stream.on('info', (info) => {
-        console.info('[DOWNLOADER]', `Downloading video: ${info.videoDetails.title}`);
-      });
-
-
+      stream.on('response', () => {});
+      stream.on('info', (info) => {});
       stream.on('end', () => {
-        console.info('[DOWNLOADER] Downloaded');
-
-
         if (fs.statSync(filePath).size > 26214400) {
           fs.unlinkSync(filePath);
-          return api.sendMessage('The file could not be sent because it is larger than 25MB. Essayez un autre', event.threadID);
+          return api.sendMessage('The file could not be sent because it is larger than 25MB.', event.threadID);
         }
-
-
         const message = {
-          body: `✅ | TsantaBot : Votre music est prête \n\n
-
-▶️ | Titre: ${video.title}
-⏰ | Duration: ${video.duration.timestamp}`,
+          body: `${music.title}`,
           attachment: fs.createReadStream(filePath)
         };
-
-
         api.sendMessage(message, event.threadID, () => {
           fs.unlinkSync(filePath);
-        });
+        }, event.messageID);
       });
-    } catch (error) {
-      console.error('[ERROR]', error);
-      api.sendMessage(' An error occurred while processing the command.', event.threadID);
     }
+  } catch (error) {
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
 };
