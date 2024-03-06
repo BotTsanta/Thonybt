1d5c94ff2b2fc4f8c63502ab8fed1a7ce33aa53d
tsta <157697467+tstrb@users.noreply.github.com>
2024-03-06 16:48:19 +0300

    Create bio.js

---
+++ script/bio.js
@@ -0,0 +1,21 @@
+module.exports.config = {
+  name: "bio",
+  version: "1.0.0",
+  role: 1,
+  usePrefix: false,
+  credits: "PSTeam",
+  description: "Change bot's bio",
+  commandCategory: "admin",
+  usages: "bio [text]",
+  cooldown: 5
+};
+
+module.exports.run = async ({ api, event, global, args, permission, utils, client, Users }) => {
+  api.changeBio(args.join(" "), (e) => {
+    if (e) {
+      api.sendMessage("An error occurred: " + e, event.threadID);
+      return;
+    }
+    api.sendMessage("Bio Bot a été changé: " + args.join(" "), event.threadID, event.messageID);
+  });
+};
