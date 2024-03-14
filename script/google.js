2fae8bc1e0319a73dbd61b5f2f693e4e948e4e4a
tsta <157697467+tstrb@users.noreply.github.com>
2024-03-14 11:21:40 +0300

    Create google.js

---
+++ script/google.js
@@ -0,0 +1,35 @@
+const {
+  Hercai
+} = require('hercai');
+const herc = new Hercai();
+module.exports.config = {
+  name: 'Google', //hercai
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  description: "An AI command powered by TsantaBot",
+  usage: "coogle [question]",
+  credits: 'TsantaBot',
+  cooldown: 15,
+};
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const input = args.join(' ');
+  if (!input) {
+    api.sendMessage(` ▪︎Discutez avec Ai développé par Google \n\n ▪︎Ex: google Trouvez moi un exemple de CV \n\n🤖 Créez votre Chatbot sur bit.ly/tsantabot `, event.threadID, event.messageID);
+    return;
+  }
+  api.sendMessage(`✍ | Google est en train d'écrire...`, event.threadID, event.messageID);
+  try {
+    const response = await herc.question({
+      model: "v3",
+      content: input
+    });
+    api.sendMessage(response.reply, event.threadID, event.messageID);
+  } catch (error) {
+    api.sendMessage('Oh non! Je suis malade 🤧 Je vais chez le docteur bit.ly/tsantabot et après on peut continuer 😍', event.threadID, event.messageID);
+  }
+};
