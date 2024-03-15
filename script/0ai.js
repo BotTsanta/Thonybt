b0f9611e9dc7f2209443d5623fee7b42b096a8a6
tstbt <158824950+BotTsanta@users.noreply.github.com>
2024-03-15 14:28:31 +0300

    Update 0ai.js

--- script/0ai.js
+++ script/0ai.js
@@ -6,7 +6,7 @@ module.exports.config = {
   hasPrefix: true,
   aliases: ['openai2'],
   description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
+  usage: "Ai [prompt]",
   credits: 'TsantaBot',
   cooldown: 10,
 };
@@ -17,7 +17,7 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`Simple Ai TsantaBot: \n ▪︎Ex: Ai Bonjour Tu es là ?\n\n * Isaky ny manontany dia asina "Ai" foana ny fiandohany`, event.threadID, event.messageID);
+    api.sendMessage(`Simple Ai : \n ▪︎Ex: Ai Bonjour Tu es là ?\n\n * Isaky ny manontany dia asina "Ai" foana ny fiandohany`, event.threadID, event.messageID);
     return;
   }
   api.sendMessage(`✍ En train de répondre...`, event.threadID, event.messageID);
